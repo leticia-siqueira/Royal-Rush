caff5a93eaa7d1aaeae7e5a3a2f8f8616e798945
Davi Maltez <maltezdavid438@gmail.com>
2026-05-25 00:54:10 -0300

    Add ranking system with linked list and update ranking file format

--- include/ranking.h
+++ include/ranking.h
@@ -1,7 +1,15 @@
 #ifndef RANKING_H
 #define RANKING_H
 
+typedef struct NoRanking {
+    char nome[30];
+    int score;
+
+    struct NoRanking *prox;
+} NoRanking;
+
 void SalvarRanking(const char *nome, int score);
 
-#endif
+void LiberarRanking();
 
+#endif
--- src/ranking.c
+++ src/ranking.c
@@ -1,16 +1,88 @@
 #include "ranking.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Ponteiro para o início da lista
+static NoRanking *inicio = NULL;
 
 void SalvarRanking(const char *nome, int score) {
-    FILE *arquivo = fopen("ranking.txt", "a");
+
+
+//Cria um novo nó
+
+    NoRanking *novo = malloc(sizeof(NoRanking));
+
+    // Verifica se a memória foi alocada
+    if (novo == NULL) {
+        printf("Erro ao alocar memória!\n");
+        return;
+    }
+
+
+//Preenche os dados
+
+    strcpy(novo->nome, nome);
+
+    novo->score = score;
+
+    novo->prox = NULL;
+
+    if (inicio == NULL) {
+        inicio = novo;
+    }
+
+    else {
+        NoRanking *atual = inicio;
+
+        while (atual->prox != NULL) {
+            atual = atual->prox;
+        }
+
+        atual->prox = novo;
+    }
+
+
+// Salva no arquivo
+
+    FILE *arquivo = fopen("ranking.txt", "w");
 
     if (arquivo == NULL) {
         printf("Erro ao abrir ranking.txt\n");
         return;
     }
 
-    fprintf(arquivo, "%s - Score: %d\n", nome, score);
+
+    NoRanking *atual = inicio;
+
+    while (atual != NULL) {
+
+        fprintf(
+            arquivo,
+            "%s - Score: %d\n",
+            atual->nome,
+            atual->score
+        );
+
+        atual = atual->prox;
+    }
 
     fclose(arquivo);
 }
 
+void LiberarRanking() {
+
+    NoRanking *atual = inicio;
+
+    while (atual != NULL) {
+
+        NoRanking *temp = atual;
+
+        atual = atual->prox;
+
+        free(temp);
+    }
+
+    inicio = NULL;
+}
